4c1ff30a0cf9cb4120cd6f1797460469669321b1
krishnaMjha <krishnamcab7@gmail.com>
2017-08-17 23:34:02 +0530

    Initial commit

---
+++ CustomSegment.framework/Headers/CustomSegment.h
@@ -0,0 +1,19 @@
+//
+//  CustomSegment.h
+//  CustomSegment
+//
+//  Created by Jha, Krishna on 5/9/17.
+//  Copyright © 2017 Jha, Krishna. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CustomSegment.
+FOUNDATION_EXPORT double CustomSegmentVersionNumber;
+
+//! Project version string for CustomSegment.
+FOUNDATION_EXPORT const unsigned char CustomSegmentVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CustomSegment/PublicHeader.h>
+
+
